ec79b1b5ceb27782cd96126a9c04403472f9c779
graydevon <devongray@Devons-MacBook-Pro.local>
2023-02-23 05:57:44 -0600

    store/tags

--- app/views.py
+++ app/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def say_hello(request): 
-    return render(request, 'front.html')
+    return render(request, 'front.html', {'name': 'Mosh'})
